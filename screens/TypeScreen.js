c63ff2fc4e2dd13b272bfe7e5c1133c7a6333ee3
yujinni <86652449+yujin37@users.noreply.github.com>
2023-04-30 02:35:49 +0900

    유형별 문제 페이지|버튼 클릭시 버튼 등장

--- screens/TypeScreen.js
+++ screens/TypeScreen.js
@@ -1,22 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { View, Text, Button, StyleSheet } from 'react-native';
+import AppNameHeader from './component/AppNameHeader';
 
-import {View, Text, Button} from 'react-native'
-import AppNameHeader from './component/AppNameHeader'
+const TypeScreen = ({ navigation }) => {
+  const [showButtons, setShowButtons] = useState(false);
 
+  const toggleButtons = () => {
+    setShowButtons(!showButtons);
+  }
 
-const TypeScreen = ({navigation}) =>{
-    return (
-        <View>
-            <AppNameHeader/>
-            <View>
-                <Text>
-                    유형별 문제
-                </Text>
-
-                <Button title = "유형학습" onPress={()=>{navigation.push("TypeQuest")}}/>
-            </View>
+  return (
+    <View>
+      <AppNameHeader />
+      <View style={styles.buttonRow}>
+        <Button title="듣기" onPress={toggleButtons} />
+        <Button title="읽기" onPress={toggleButtons} />
+        <Button title="쓰기" onPress={toggleButtons} />
+      </View>
+      {showButtons && (
+        <View style={styles.buttonColumn}>
+          <Button title="Button 1" onPress={() => { console.log("Button 4 pressed") }} />
+          <Button title="Button 2" onPress={() => { console.log("Button 5 pressed") }} />
+          <Button title="Button 3" onPress={() => { console.log("Button 6 pressed") }} />
         </View>
-    );
+      )}
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  buttonRow: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginBottom: 20,
+  },
+  buttonColumn: {
+    flexDirection: 'column',
+  },
+});
 
 export default TypeScreen;
