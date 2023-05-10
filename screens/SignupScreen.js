b19641729bda22221c0db2a567fd2ac014a42a47
yujinni <86652449+yujin37@users.noreply.github.com>
2023-05-10 17:22:21 +0900

    로그인 | 회원가입 및 로그인 파이어베이스 구현

--- screens/SignupScreen.js
+++ screens/SignupScreen.js
@@ -1,29 +1,56 @@
 import React, {useState} from 'react';
+import {Alert} from 'react-native';
+import {signUp } from "../lib/auth";
 
 import {Button, View ,TextInput, StyleSheet} from 'react-native'
 import AppNameHeader from './component/AppNameHeader'
 
 
 const SignupScreen = ({navigation}) =>{
-    const [id, onChangeId] = React.useState('');
-    const [pw, onChangePw] =React.useState('');
-
+    //const [id, onChangeId] = React.useState('');
+    //const [pw, onChangePw] =React.useState('');
+    //const [pwck, onChangePwck] =React.useState('');
+    const [form, setForm] = useState({
+        email: "",
+        password: "",
+        
+    });
+    const resultMessages = {
+        "auth/email-already-in-use": "이미 가입된 이메일입니다.",
+        "auth/wrong-password": "잘못된 비밀번호입니다.",
+        "auth/user-not-found": "존재하지 않는 계정입니다.",
+        "auth/invalid-email": "유효하지 않은 이메일 주소입니다."
+    }
+    const signUpSubmit = async () => { // 회원가입 함수
+        const {email, password} = form;
+        const info = {email, password};
+        try {
+          const {user} = await signUp(info);
+          console.log(user);
+        } catch (e) {
+            const alertMessage = resultMessages[e.code] ? 
+            resultMessages[e.code] : "알 수 없는 이유로 회원가입에 실패하였습니다.";
+            Alert.alert("회원가입 실패", alertMessage);
+        }
+    }
+    
     return (
         <View>
             <AppNameHeader/>
             <View>
                 <TextInput
                     style={styles.input}
-                    onChangeText={onChangeId}
-                    value={id}
+                    onChangeText={(value)=>setForm({email: value, password: form.password})}
+                    value={form.email}
                 />
                 <TextInput
                     style={styles.input}
-                    onChangeText={onChangePw}
-                    value={pw}
+                    onChangeText={(value)=>setForm({email: form.email, password: value})}
+                    value={form.password}
                 />
                 
-                <Button title = "회원가입" onPress={()=> navigation.navigate("Signin")}/> 
+                
+                <Button title = "회원가입" onPress={()=> signUpSubmit()}/> 
             </View>
         </View>
     );
