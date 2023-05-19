6d145023c094aa3eb84ced49db9488d8f6a6a4c7
yujinni <86652449+yujin37@users.noreply.github.com>
2023-05-19 10:22:19 +0900

    내 정보 | 로그인 후 이메일 화면 출력

--- screens/InfoScreen.js
+++ screens/InfoScreen.js
@@ -1,10 +1,18 @@
-import React from 'react';
-
+import React, {useState, useEffect} from 'react';
+import {subscribeAuth } from "../lib/auth";
 import {View, Text} from 'react-native'
 import AppNameHeader from './component/AppNameHeader'
 
 
 const InfoScreen = () =>{
+    const [userEmail, setUserEmail] = useState(null);
+    const handleAuthStateChanged =(user) =>{
+        if(user){
+            console.log('로그인', user.email);
+            setUserEmail(user.email);
+        }
+    }
+    const unsubscribe = subscribeAuth(handleAuthStateChanged);
     return (
         <View>
             <AppNameHeader/>
@@ -12,6 +20,8 @@ const InfoScreen = () =>{
                 <Text>
                     내 정보
                 </Text>
+                <Text> {userEmail} </Text>
+                
             </View>
         </View>
     );
