8fb7cb21f26468e1f5ecd74b447747c91e59c3c5
yujinni <86652449+yujin37@users.noreply.github.com>
2023-05-26 20:09:52 +0900

    내 정보 | 설정-앱 정보 추가

---
+++ screens/InfoApp.js
@@ -0,0 +1,58 @@
+import React,{useState} from 'react';
+import {Text,View, StyleSheet} from 'react-native';
+const InfoApp = () => {
+    const [isExpanded, setIsExpanded] = useState(false);
+    const [isExpanded2, setIsExpanded2] = useState(false);
+
+    const toggleContent = () => {
+        setIsExpanded(!isExpanded);
+    };
+    const toggleContent2 = () => {
+        setIsExpanded2(!isExpanded2);
+    };
+
+    return (
+      <View>
+        <Text style={styles.title}> 앱 정보 </Text>
+        <Text style={styles.version}> 버전 정보</Text>
+        <Text style={styles.versionContent}> version 0.0.1</Text>
+        <Text style={styles.toggleTitle} onPress={toggleContent}> -서비스 이용 안내-  </Text>
+        {isExpanded && (
+        <View style={styles.contentContainer}>
+          <Text>펼쳐진 내용 1</Text>
+          <Text>펼쳐진 내용 2</Text>
+        </View>
+        )}
+        <Text style={styles.toggleTitle} onPress={toggleContent2}> -앱 관련 라이선스-  </Text>
+        {isExpanded2 && (
+        <View style={styles.contentContainer}>
+          <Text>펼쳐진 내용 11</Text>
+          <Text>펼쳐진 내용 22</Text>
+        </View>
+        )}
+      </View>
+    ); 
+};
+const styles = StyleSheet.create({
+    title:{
+        fontSize: 30,
+        fontWeight: 'bold'
+        
+    },
+    version:{
+        fontSize: 20,
+    },
+    versionContent:{
+        fontSize: 15,
+    },
+    toggleTitle: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        marginTop: 10,
+    },
+    
+    contentContainer: {
+        marginTop: 10,
+    },
+})
+export default InfoApp;
