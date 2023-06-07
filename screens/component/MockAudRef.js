caacc398428c49691137230535bbbdad2aa0b09e
jiwoo <jwo2955@gmail.com>
2023-06-07 20:17:08 +0900

    오디오 파일 재생

--- screens/component/MockAudRef.js
+++ screens/component/MockAudRef.js
@@ -1,14 +1,16 @@
 import React, { useEffect, useState } from 'react';
 import { StyleSheet, View, Text, TouchableOpacity } from 'react-native';
 
-// import Sound from 'react-native-sound';
+import Sound from 'react-native-sound';
 
-const MockAudRef = ({ source }) => {
-    
-    const [isPlaying, setIsPlaying] = useState(false);
+Sound.setCategory('Playback');
+
+var audio = null;
+
+const MockAudRef = ({ source, isPlaying, setIsPlaying }) => {
 
     
-    const audio = new Sound(source, null, (err) => {
+    audio = new Sound(source, Sound.MAIN_BUNDLE, (err) => {
         if (err) {
             console.log('failed to load the sound', error);
             return;
@@ -16,52 +18,45 @@ const MockAudRef = ({ source }) => {
         
         // 로드 성공
         console.log('duration in seconds: ' + audio.getDuration() + 'number of channels: ' + audio.getNumberOfChannels());
-        
-        // 반복 재생 없음
-        audio.setNumberOfLoops(-1);
-        
-    });
+        console.log('isPlaying: ', audio.isPlaying());
 
+        // 반복 재생 없음
+        audio.setNumberOfLoops(0);
+
+        new Sound().pa
+    });
+    
+    
     const audioControl = () => {
-        if (isPlaying) {
-            audio.pause((err) => {
-                if (err) {
-                    console.log('일시 정지 실패');
-                    return;
-                }
-            });
-            setIsPlaying(false);
-        } else {
-            audio.play((err) =>{
-                if (err) {
-                    console.log('재생 실패');
-                    return;
+        if (audio.isPlaying()) {
+            audio.pause((success) => {
+                if (success) {
+                    console.log('successfully finished playing');
+                } else {
+                    console.log('playback failed due to audio decoding errors');
                 }
             })
-            setIsPlaying(true);
+        } else {
+            audio.play((success) => {
+                if (success) {
+                    console.log('successfully finished playing');
+                } else {
+                    console.log('playback failed due to audio decoding errors');
+                }
+            });
         }
     }
-
-    const play = () => {
-        audio.play();
-        setIsPlaying(true);
-        console.log(`재생 중`);
-    }
-
-    const pause = () => {
-        audio.pause();
-        setIsPlaying(false);
-        console.log('일시 정지');
-    }
+    
         
     return (
         <View>
-            <TouchableOpacity onPress={() => {audioControl()}}>
+            <TouchableOpacity onPress={() => audioControl()}>
+                <Text>Hello</Text>
                 <Text>
                     {
-                        isPlaying
-                        ? 'Playing'
-                        : 'Pause'
+                        audio.isPlaying()
+                        ? '멈추기'
+                        : '재생하기'
                     }
                 </Text>
             </TouchableOpacity>
--- screens/component/MockProbModal.js
+++ screens/component/MockProbModal.js
@@ -1,59 +1,145 @@
-import React from 'react';
-import { View, Text, Modal, Button, StyleSheet, TouchableOpacity } from 'react-native'
+import React, { useState } from 'react';
+import { View, Text, Image, Modal, Button, StyleSheet, TouchableOpacity } from 'react-native'
 
-const MockProbModal = ({ problem, modal, setModal }) => {
+const MockProbModal = ({ problem, index, setVisible, images, audios }) => {
+
   
-  console.log(`problem: ${problem}`);
 
   return (    
-    <Modal
-      animationType={"slide"}
-      transparent={false}
-      visible={modal.open}
-      onRequestClose={() => {
-          setModal({open: false})
-          console.log("modal appearance")
+      <Modal
+        animationType={"slide"}
+        transparent={false}
+        visible={true}
+        onRequestClose={() => {
+            setVisible(false)
+            console.log("modal appearance")
       }}>
-      <View>
+      <View style={styles.container}>
 
         {/* ProbMain */}
         <View style={styles.probMain}>
-            <Text>{ problem.PRB_NUM }</Text>
-            <Text>{ problem.PRB_MAIN_CONT}</Text>
+            <Text style={styles.probMainCnt}>Q{ problem[index].PRB_NUM } { problem[index].PRB_MAIN_CONT}</Text>
         </View>
 
+        {/* 이미지 */}
+        {
+          problem[index].IMG_REF in images
+          ? <Image
+            style={{ width: 100, height: 100 }}
+            source={{ uri: images[problem[index].IMG_REF].url }}
+          />
+          : null
+        }
 
-        <Text>{ problem.PRB_TXT }</Text>
-        <Text>{ problem.PRB_SUB_CONT }</Text>
+        <Text>{ problem[index].PRB_TXT }</Text>
+        <Text>{ problem[index].PRB_SUB_CONT }</Text>
 
         {
-          problem.PRB_SECT === "듣기" || problem.PRB_SECT === "읽기"
+          problem[index].PRB_SECT === "LS" || problem[index].PRB_SECT === "RD"
           ? <View>
-              <TouchableOpacity disabled={true} style={[styles.choiceButton, {backgroundColor: problem.USER_CHOICE === "1" ? "#BBD6B8" : "#D9D9D9"}]}>
-                <Text>{ problem.PRB_CHOICE1 }</Text>
+              {
+                problem[index].PRB_CHOICE1 in images
+                ? <TouchableOpacity disabled={true} style={[styles.choiceImgConainer]}>
+                  <Image
+                    style={[styles.choiceImg, 
+                      {borderColor: problem[index].PRB_CORRT_ANSW === "1" 
+                      ? "#BAD7E9" 
+                      : (
+                        problem[index].USER_CHOICE === "1"
+                        ? "#FFACAC"
+                        : "#D9D9D9"
+                        )}]}
+                    source={{uri: images[problem[index].PRB_CHOICE1].url}}
+                  />
+                </TouchableOpacity>
+                : <TouchableOpacity disabled={true} style={[styles.choiceButton, 
+                    {
+                      backgroundColor: problem[index].PRB_CORRT_ANSW === "1" 
+                      ? "#BAD7E9" 
+                      : (
+                        problem[index].USER_CHOICE === "1"
+                        ? "#FFACAC"
+                        : "#D9D9D9"
+                        )
+                    }]}>
+                    <Text>{ problem[index].PRB_CHOICE1 }</Text>
+                </TouchableOpacity>
+              }
+              <TouchableOpacity disabled={true} style={[styles.choiceButton, 
+                {
+                  backgroundColor: problem[index].PRB_CORRT_ANSW === "2" 
+                  ? "#BAD7E9" 
+                  : (
+                    problem.USER_CHOICE === "2"
+                    ? "#FFACAC"
+                    : "#D9D9D9"
+                    )
+                }]}>
+              {
+                problem[index].PRB_CHOICE2 in images
+                ? <Image
+                  style={{width: 50, height: 50}}
+                  source={{uri: images[problem[index].PRB_CHOICE2].url}}
+                />
+                : <Text>{ problem[index].PRB_CHOICE2 }</Text>
+              }
               </TouchableOpacity>
-              <TouchableOpacity disabled={true} style={[styles.choiceButton, {backgroundColor: problem.USER_CHOICE === "2" ? "#BBD6B8" : "#D9D9D9"}]}>
-                <Text>{ problem.PRB_CHOICE2 }</Text>
+              <TouchableOpacity disabled={true} style={[styles.choiceButton, {
+                  backgroundColor: problem[index].PRB_CORRT_ANSW === "3" 
+                  ? "#BAD7E9" 
+                  : (
+                    problem[index].USER_CHOICE === "3"
+                    ? "#FFACAC"
+                    : "#D9D9D9"
+                    )
+                }]}>
+              {
+                problem[index].PRB_CHOICE3 in images
+                ? <Image
+                  style={{width: 50, height: 50}}
+                  source={{uri: images[problem[index].PRB_CHOICE3].url}}
+                />
+                : <Text>{ problem[index].PRB_CHOICE3 }</Text>
+              }
               </TouchableOpacity>
-              <TouchableOpacity disabled={true} style={[styles.choiceButton, {backgroundColor: problem.USER_CHOICE === "3" ? "#BBD6B8" : "#D9D9D9"}]}>
-                <Text>{ problem.PRB_CHOICE3 }</Text>
-              </TouchableOpacity>
-              <TouchableOpacity disabled={true} style={[styles.choiceButton, {backgroundColor: problem.USER_CHOICE === "4" ? "#BBD6B8" : "#D9D9D9"}]}> 
-                <Text>{ problem.PRB_CHOICE4 }</Text>
+              <TouchableOpacity disabled={true} style={[styles.choiceButton, {
+                  backgroundColor: problem[index].PRB_CORRT_ANSW === "4" 
+                  ? "#BAD7E9" 
+                  : (
+                    problem[index].USER_CHOICE === "4"
+                    ? "#FFACAC"
+                    : "#D9D9D9"
+                    )
+                }]}> 
+              {
+                problem[index].PRB_CHOICE4 in images
+                ? <Image
+                  style={{width: 50, height: 50}}
+                  source={{uri: images[problem[index].PRB_CHOICE4].url}}
+                />
+                : <Text>{ problem[index].PRB_CHOICE4 }</Text>
+              }
               </TouchableOpacity>
           </View>
           : <View>
-            <Text>{ problem.PRB_NUM }</Text>
+            <Text>{ problem[index].PRB_NUM }</Text>
             <Text>점수가 여기 표시됩니다.</Text>
-            <Text>{ problem.USER_CHOICE }</Text>
-            <Text>{ problem.PRB_CORRT_ANSW }</Text>
+            <Text>{ problem[index].USER_CHOICE }</Text>
+            <Text>{ problem[index].PRB_CORRT_ANSW }</Text>
           </View>
         }
 
-        <View style = {styles.probMain}>
-          <Button onPress = {() => {setModal({open: false})}} title={"CLOSE"} />
+        <View style = {styles.controlButttonContainer}>
+          <TouchableOpacity 
+            onPress = {() => {setVisible(false)}} 
+            style = {[styles.controlButton, 
+              {
+                backgroundColor: '#94AF9F',
+                width: 200
+              }]}>
+            <Text>CLOSE</Text>
+          </TouchableOpacity>
         </View>
-
       </View>
     </Modal>
   );
@@ -61,14 +147,69 @@ const MockProbModal = ({ problem, modal, setModal }) => {
 };
 
 const styles = StyleSheet.create({
-    probMain: {
+    
+    container:{
+        padding: 20,
+    },
+
+    containerPos: {
+        flex: 20
+    },
+
+    probMainCnt: {
+        fontSize: 17,
+    },
+
+    controlButtonprobMain: {
         flexDirection: 'row'
     },
     choiceButton: {
       alignItems: "center",
       borderRadius: 10,
       padding: 16
-    }
+    },
+
+
+    probMainContainer: {
+        flexDirection: 'row'
+    },
+
+    selectSection: {
+        flex: 1,
+        justifyContent: 'space-between'
+    },
+
+    choiceButton: {
+        alignItems: "center",
+        borderRadius: 10,
+        padding: 16,
+        marginVertical: 10,
+    },
+
+
+    choiceImgConainer: {
+        alignItems: 'center',
+        marginVertical: 10,
+    },
+    choiceImg: {
+        width: 100,
+        height: 100,
+        borderStyle: 'solid',
+        borderWidth: 5,
+    },
+
+
+    controlButttonContainer: {
+        justifyContent: 'center',
+        flexDirection: 'row'
+    },
+    controlButton: {
+        alignItems: "center",
+        borderRadius: 10,
+        padding: 16,
+        width: 100, 
+        marginHorizontal: 10
+    },
 })
 
 export default MockProbModal;
