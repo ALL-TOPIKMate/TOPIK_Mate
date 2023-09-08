7a70726173f6cc8c8bf6961ba45cf85cd0649868
rladbwlsdlwl <rladbwlsdlwl@naver.com>
2023-09-08 19:12:17 +0900

    오디오 느리게 재생기능 추가

--- screens/component/AudRef.js
+++ screens/component/AudRef.js
@@ -1,73 +1,152 @@
+import { transform } from '@babel/core';
 import React, { useRef, useState, useEffect } from 'react';
-import {View, Text, StyleSheet, Button, TouchableOpacity} from 'react-native'
+import { View, Text, StyleSheet, Button, TouchableOpacity } from 'react-native'
 
 
 
-const AudRef = ({ audio }) =>{
+const AudRef = ({ audio }) => {
 
     const [isRunning, setIsRunning] = useState(false);
-    
-    useEffect(()=>{
+    const [isSlowRunning, setIsSlowRunning] = useState(false);
+
+
+    useEffect(() => {
         setIsRunning(false)
+        setIsSlowRunning(false)
     }, [audio])
 
-    function audioPlay(){
-        if(audio){
-            
-            if(audio.isPlaying()){
-                audio.pause()
 
+    // 오디오 재생 / 스탑 함수
+    // 오디오 속도 조절시 자동 재생됨 -> 오디오 중단
+    function audioPlay() {
+        setIsSlowRunning(false)
+
+        if (audio) {
+            if (audio.isPlaying()) {
+
+                audio.pause()
                 setIsRunning(false)
-            }else{
+
+            } else {
+
                 setIsRunning(true)
-        
-                // audio.play()
+                audio.setSpeed(1)
+                audio.pause()
 
                 audio.play((success) => {
                     if (success) {
-                      setIsRunning(false);
-                      console.log('successfully finished playing');
+                        setIsRunning(false);
+                        console.log('successfully finished playing');
                     } else {
-                      setIsRunning(false);
-                      console.log('playback failed due to audio decoding errors');
+                        setIsRunning(false);
+                        console.log('playback failed due to audio decoding errors');
                     }
                 })
             }
         }
     }
 
-    return (
-        <View style = {styles.btnBox}>
-            <TouchableOpacity onPress={()=>{audioPlay()}} style = {styles.btnPlay}>
-                    {
-                        isRunning
-                        ? <Text style = {{color: "#F6F1F1", fontSize: 20, textAlign: "center"}}>
-                        Stop
-                        </Text>
-                        : <Text style = {{color: "#F6F1F1", fontSize: 20, textAlign: "center"}}>
-                        Start
-                        </Text>
+
+    // 오디오 재생 / 스탑 함수
+    // 오디오 속도 조절시 자동 재생됨 -> 오디오 중단
+    function audioSlowPlay() {
+        setIsRunning(false)
+
+        if (audio) {
+
+            if (audio.isPlaying()) {
+
+                audio.pause()
+                setIsSlowRunning(false)
+
+            } else {
+
+                setIsSlowRunning(true)
+                audio.setSpeed(0.75)
+                audio.pause()
+
+                audio.play((success) => {
+                    if (success) {
+                        setIsSlowRunning(false);
+                        console.log('successfully finished playing');
+                    } else {
+                        setIsSlowRunning(false);
+                        console.log('playback failed due to audio decoding errors');
                     }
+                })
+            }
+        }
+    }
+
+
+    return (
+        <View style={styles.btnBox}>
+            <TouchableOpacity onPress={() => { audioPlay() }} style={styles.btnPlay}>
+                {
+                    isRunning
+                        ? <View style={styles.stopButton} />
+                        : <View style={styles.startButton} />
+                }
             </TouchableOpacity>
-        </View>  
+            
+            <TouchableOpacity onPress={() => { audioSlowPlay() }} style={[styles.btnPlay, {transform: [{scale: 0.65}], marginTop: 20}]}>
+                <Text style = {{color: "#F6F1F1", fontSize: 12, left: 28, bottom: isSlowRunning ? 14 : 8}}>
+                    Slow
+                </Text>
+                
+                {
+                    isSlowRunning
+                    ? <View style={[styles.stopButton, {bottom: 8}]} />
+                    : <View style={[styles.startButton, {bottom: 8}]} />
+                }
+            </TouchableOpacity>
+        </View>
     )
 }
 
 const styles = StyleSheet.create({
-    btnBox:{
-        backgroundColor: "#D9D9D9", 
-        flexDirection: "row", 
+    btnBox: {
+        backgroundColor: "#D9D9D9",
+        flexDirection: "row",
         justifyContent: "center",
 
         alignItems: "center",
 
-        paddingVertical: 30
-    }, 
+        height: 160
+    },
 
-    btnPlay:{
+    btnPlay: {
         backgroundColor: "#94AF9F",
-        padding: 30,
-        borderRadius: 20
+        width: 100,
+        height: 100,
+        borderRadius: 20,
+
+        alignItems: "center",
+        justifyContent: "center"
+    },
+
+    // triangle shape
+    startButton: {
+        width: 0,
+        height: 0,
+        backgroundColor: "transparent",
+        borderStyle: "solid",
+        borderLeftWidth: 25,
+        borderRightWidth: 25,
+        borderBottomWidth: 50,
+        borderLeftColor: "transparent",
+        borderRightColor: "transparent",
+        borderBottomColor: "#F6F1F1",
+
+        transform: [{ rotate: "90deg" }],
+
+        left: 5
+    },
+    // rectangle shape
+    stopButton: {
+        width: 40,
+        height: 40,
+        backgroundColor: "#F6F1F1"
     }
 })
 
